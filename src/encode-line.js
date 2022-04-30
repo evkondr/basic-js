56500f0a88d3dc7ebfd9b439981e3fb44fb5f52e
evkondr <kondratiev.evg@gmail.com>
2022-04-30 19:46:05 +0300

    feat: added some ST functions

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let encodedStr=''
+  let factor = 1
+  for(let i=0; i<str.length; i++){
+     if(str[i] == str[i+1]){
+       factor++
+     }else{
+       factor = factor==1?'':factor
+       encodedStr+=factor+str[i]
+       factor = 1
+     }
+  }
+  return encodedStr
 }
 
 module.exports = {
--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let newArrOfNames = []
+  let suffix=1
+  for (let name of names){
+    let copy = name;
+    while(newArrOfNames.indexOf(copy) !== -1){
+      copy = `${name}(${suffix})`
+      suffix++
+    }
+    newArrOfNames.push(copy)
+    suffix=1
+  }
+  return newArrOfNames
 }
 
 module.exports = {
